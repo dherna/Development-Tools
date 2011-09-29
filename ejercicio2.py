2d34a1e1e6c218dab70228d6958c27635730e55f
donaldyo <diego.herna@gmail.com>
2011-09-30 00:23:07 +0200

    ejercicio 2 terminado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-usuario=raw_input("¿Qué usuairo comprobamos?")
+usuario=raw_input("Que usuairo comprobamos?")
 
 try:
-	fichero = file('/etc/passwd','r')
+   fichero = file('/etc/passwd','r')
 except IOError:
-	print "Esto no es un sistama Linux"
+   print "Esto no es un sistama Linux"
 else:
+   shell={}
    while True:
      linea = fichero.readline()
      if not linea: break
      dato = linea.split(":")
-     #shell=
-    if shell.has_key(usuario):
-        print "El usuario " + usuario + " tiene el shell: " + shell[usuario]
-    else:
-        print "El usuario " + usaurio + " no está en el sistema"
+     shell[dato[0]] = dato[6]
+   if shell.has_key(usuario):
+     print "El usuario " + usuario + " tiene el shell: " + shell[usuario]
+   else:
+     print "El usuario " + usuario + " no esta en el sistema"
 
 print "fin del ejercicio2" 
 
---
+++ prueba_raw.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+nombre=raw_input("Dime un usuairo")
+print  "Hola "+nombre
+
