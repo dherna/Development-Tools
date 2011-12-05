3a905812525ef56eb569b9af8062376493d04bac
donaldyo <diego.herna@gmail.com>
2011-12-05 23:50:51 +0100

    Empezar proyecto crawer, prueba de carpeta.

---
+++ crawer/setup.py
@@ -0,0 +1,7 @@
+name:	crawer
+version: 0.1
+Description: scaneador web
+long_description: Es un programa para analizar las web dependiendo del nivel que se le pida.
+author: Diego Hernández
+author_email: diego.herna@gmail.com
+license: GNU/GPL v3 or later 
